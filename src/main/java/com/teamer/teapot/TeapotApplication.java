2d05cc096ddb016dd475b6850f5d28472c96b4b6
TanzJ <bbcckkl@live.com>
2020-07-07 16:15:47 +0800

    init-project

---
+++ src/main/java/com/teamer/teapot/TeapotApplication.java
@@ -0,0 +1,13 @@
+package com.teamer.teapot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeapotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeapotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamer/teapot/TeapotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamer.teapot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeapotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
