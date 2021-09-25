d542eff2af3a0979420de85e858de52c3301b85c
TanzJ <im.tanzj@live.com>
2021-09-25 23:37:52 +0800

    feature: 支持获取applicationContext

---
+++ teapot-core/src/main/java/com/teamer/teapot/core/utils/SpringContextHolder.java
@@ -0,0 +1,25 @@
+package com.teamer.teapot.core.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tanzj
+ * @date 2021/9/25
+ */
+@Component
+public class SpringContextHolder implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+}
