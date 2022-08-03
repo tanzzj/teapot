5402833b8b992db9d4a22e62601d95440162da32
TanzJ <im.tanzj@live.com>
2022-08-03 23:47:50 +0800

    optimize: 支持操作符

--- teapot-strategy/src/main/java/com/teamer/teapot/strategy/model/bo/ConditionBO.java
+++ teapot-strategy/src/main/java/com/teamer/teapot/strategy/model/bo/ConditionBO.java
@@ -15,9 +15,41 @@ public class ConditionBO {
 
     private Long id;
     private FactorBO factor;
-    private String operation;
+    private OperationType operation;
     private Object value;
     private Boolean multiValue;
     private Map<String, Object> extend;
+
+    public enum OperationType {
+        /**
+         * 等于
+         */
+        EQUAL("==", "等于"),
+
+        NOT_EQUAL("!=", "不等于"),
+
+        LESS_THAN("<", "小于"),
+
+        LESS_THAN_OR_EQUAL_TO("<=", "小于等于"),
+
+        GREATER_THAN(">", "大于"),
+
+        GREATER_THAN_OR_EQUAL_TO(">=", "大于等于"),
+
+        IN("in", "大于等于"),
+
+        NOT_IN("not in", "大于等于"),
+
+
+        ;
+
+        String desc;
+        String operation;
+
+        OperationType(String operation, String desc) {
+            this.desc = desc;
+            this.operation = operation;
+        }
+    }
 
 }
